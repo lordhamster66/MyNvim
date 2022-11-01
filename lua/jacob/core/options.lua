1ca5f634a9f2cf5659820a48bcdb83995c07a0f5
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2022-11-01 11:43:06 +0800

    add some auto options

--- lua/jacob/core/options.lua
+++ lua/jacob/core/options.lua
@@ -62,8 +62,21 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
 
+
+-- Auto save
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 	pattern = { "*" },
 	command = "silent! wall",
 	nested = true,
+})
+
+-- Restore cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.fn.setpos(".", vim.fn.getpos("'\""))
+            vim.cmd("silent! foldopen")
+        end
+    end,
 })
