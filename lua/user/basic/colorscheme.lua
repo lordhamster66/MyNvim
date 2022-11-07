e846cab129e6c56fc2904898754853730a281b61
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2022-11-07 17:28:29 +0800

    update colorscheme conf

--- lua/user/basic/colorscheme.lua
+++ lua/user/basic/colorscheme.lua
@@ -3,7 +3,7 @@ local setup, theme = pcall(require, "tokyonight")
 if setup then
 	theme.setup({
 		style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-		transparent = true, -- Enable this to disable setting the background color
+		transparent = false, -- Enable this to disable setting the background color
 		terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 		styles = {
 			comments = { italic = true },
