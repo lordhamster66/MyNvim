6e2a4a2c58096a9a89bfdd571f05900e4f62ce52
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-02-16 13:56:50 +0800

    update lualine conf

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -4,30 +4,6 @@ if not status then
 	return
 end
 
--- get lualine nightfly theme
-local lualine_nightfly = require("lualine.themes.nightfly")
-
--- new colors for theme
-local new_colors = {
-	blue = "#65D1FF",
-	green = "#3EFFDC",
-	violet = "#FF61EF",
-	yellow = "#FFDA7B",
-	black = "#000000",
-}
-
--- change nightlfy theme colors
-lualine_nightfly.normal.a.bg = new_colors.blue
-lualine_nightfly.insert.a.bg = new_colors.green
-lualine_nightfly.visual.a.bg = new_colors.violet
-lualine_nightfly.command = {
-	a = {
-		gui = "bold",
-		bg = new_colors.yellow,
-		fg = new_colors.black, -- black
-	},
-}
-
 local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
@@ -81,8 +57,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		-- theme = lualine_nightfly,
-		theme = "tokyonight",
+		theme = "kanagawa",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
