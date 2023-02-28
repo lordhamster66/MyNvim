183872165f6fc6c1d17d7f8efbc398390370cd0d
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-02-28 13:37:25 +0800

    update python and typescript snippet

--- lua/user/plugins/snip/snippets/python.lua
+++ lua/user/plugins/snip/snippets/python.lua
@@ -1,20 +1,17 @@
 local ls = require("luasnip")
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-
--- splits the string of the comma separated argument list into the arguments
--- and returns the text-/insert- or restore-nodes
+local snip = ls.snippet
+local text = ls.text_node
+local insert = ls.insert_node
 
 -- create the actual snippet
 local snippets = {
-	s("shebang", {
-		t({ "#!/usr/bin/env python", "" }),
-		i(0),
+	snip("env", {
+		text({ "#!/usr/bin/env python", "" }),
+		insert(0),
 	}),
-	s("ti", {
-		t({ "# type: ignore" }),
-		i(0),
+	snip("ti", {
+		text({ "# type: ignore" }),
+		insert(0),
 	}),
 }
 
