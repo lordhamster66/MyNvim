ca6e0454cac40ed4c105ec2dc8ac4ac6a41703cd
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-06-02 10:46:08 +0800

    update telescope conf

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -15,12 +15,15 @@ local mappings = {
 	i = {
 		["<C-j>"] = actions.move_selection_next,
 		["<C-k>"] = actions.move_selection_previous,
+		["<C-\\>"] = actions.file_vsplit,
 	},
 	-- normal mode
 	n = {
 		["l"] = actions.toggle_selection,
 		["L"] = actions.select_all,
 		["H"] = actions.drop_all,
+		["-"] = actions.file_split,
+		["\\"] = actions.file_vsplit,
 		["<C-j>"] = actions.move_selection_next,
 		["<C-k>"] = actions.move_selection_previous,
 		["<C-q>"] = actions.smart_send_to_qflist,
