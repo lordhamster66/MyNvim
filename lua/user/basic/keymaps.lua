b2ecdcb0ce2edfa85f5b829eca5a26f5c4124d74
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-07-21 14:03:19 +0800

    update keymaps

--- lua/user/basic/keymaps.lua
+++ lua/user/basic/keymaps.lua
@@ -21,6 +21,10 @@ keymap.set("n", "<C-d>", "9jzz")
 -- Window
 keymap.set("n", "<C-w>\\", ":vsp<CR>")
 keymap.set("n", "<C-w>-", ":sp<CR>")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-l>", "<C-w>l")
 
 ----------------------
 -- Plugin Keybinds
