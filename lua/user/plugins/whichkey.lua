66fb1b2061b1be2cb9bfffa1c764ef354a3b106b
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-08-15 14:11:52 +0800

    update whichkey

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -86,12 +86,9 @@ local mappings = {
 	["q"] = { "<cmd>:qa<CR>", "Quit Nvim" },
 	["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
 	["o"] = { "<cmd>Lspsaga outline<CR>", "Outline" },
+	["t"] = { "<cmd>Lspsaga term_toggle<CR>", "Term Toggle" },
 	["x"] = { "<cmd>:Bdelete!<CR>", "Close Buffer" },
 
-	t = {
-		name = "Tab",
-		n = { "<cmd>tabnew<CR>", "New tab!" },
-	},
 	b = {
 		name = "Buffer",
 		b = { "<cmd>%bd|e#|bd#<CR>", "Close all buffer except this one!" },
