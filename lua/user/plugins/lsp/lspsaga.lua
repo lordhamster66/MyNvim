0a2c0712a63d005a423376f67d4d64619738fb33
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-08-17 16:22:50 +0800

    update server

--- lua/user/plugins/lsp/lspsaga.lua
+++ lua/user/plugins/lsp/lspsaga.lua
@@ -17,7 +17,8 @@ saga.setup({
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts) -- show definition, references
-vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
 vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 vim.keymap.set("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
