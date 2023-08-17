ba139f4a0ece6bf831a2dadfbf4958368c80cb46
zhaojinbiao <zhaojinbiao@zhicui.com.cn>
2023-08-17 16:17:07 +0800

    update

--- lua/user/plugins/lsp/lspsaga.lua
+++ lua/user/plugins/lsp/lspsaga.lua
@@ -10,7 +10,7 @@ saga.setup({
 	},
 	outline = {
 		keys = {
-			expand_or_jump = "l",
+			toggle_or_jump = "l",
 		},
 	},
 })
@@ -20,5 +20,4 @@ vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts) -- show definition, r
 vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
 vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("i", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 vim.keymap.set("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
